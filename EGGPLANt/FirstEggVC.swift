9355db2caa74a24267565717ee4411d922636050
D LI A IV <luvhalvorson@users.noreply.github.com>
2017-09-06 20:43:38 +0800

    initial commit

---
+++ EGGPLANt/FirstEggVC.swift
@@ -0,0 +1,35 @@
+//
+//  FirstEggVC.swift
+//  EGGPLANt
+//
+//  Created by YiChin Lee on 06/09/2017.
+//  Copyright © 2017 FifthGroup. All rights reserved.
+//
+
+import UIKit
+
+class FirstEggVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ EGGPLANt/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  EGGPLANt
+//
+//  Created by YiChin Lee on 06/09/2017.
+//  Copyright © 2017 FifthGroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func LoginBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func guestBtn(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

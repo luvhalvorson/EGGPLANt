3e019a458003762a7d0d973683dedf9696cc58b9
D LI A IV <luvhalvorson@users.noreply.github.com>
2017-09-11 07:38:24 +0800

    Add demo used picture to Profile scene Add basic picker view to CreateEvent scene to as a temporary substitute to drop down menu.

--- EGGPLANt/CreateEventVC.swift
+++ EGGPLANt/CreateEventVC.swift
@@ -8,30 +8,89 @@
 
 import UIKit
 
-class CreateEventVC: UIViewController {
-    @IBOutlet weak var eventTf: UITextField!
+class CreateEventVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    // These strings are for the titles of each row in pickerView
+    
+    var alertString = ["None", "At time of event","30 mins before","1 day before","1 week before"]
+    
+    var repeatString = ["Never", "Every day","Every week","Every month","Every year"]
+    
+    @IBOutlet weak var alertPV: UIPickerView!
+    @IBOutlet weak var repeatPV: UIPickerView!
+    
+    @IBAction func alertBtn(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        if sender.isSelected {
+            alertPV.isHidden = false
+        }
+        else {
+            alertPV.isHidden = true
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return 120
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 26
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == alertPV {
+            return alertString.count
+        } else {
+            
+            return 1
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let pickerLabel = UILabel()
+        
+        if pickerView == alertPV {
+            
+            let titleData = alertString[row]
+            let myTitle = NSAttributedString(string: titleData, attributes: [NSAttributedStringKey.font:UIFont(name: "Georgia", size: 16.0)!,NSAttributedStringKey.foregroundColor:UIColor.black])
+            pickerLabel.attributedText = myTitle
+            
+        } else if pickerView == repeatPV {
+            let titleData = repeatString[row]
+            let myTitle = NSAttributedString(string: titleData, attributes: [NSAttributedStringKey.font:UIFont(name: "Georgia", size: 16.0)!,NSAttributedStringKey.foregroundColor:UIColor.black])
+            pickerLabel.attributedText = myTitle
+        }
+        pickerLabel.backgroundColor = UIColor.white
+        return pickerLabel
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        alertPV.isHidden = true
+        repeatPV.isHidden = true
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
